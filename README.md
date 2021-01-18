9ea2d28a9df863ffd28a050e82dc87ef5660ddde
deadshell <deadshell@gridlan.com>
2021-01-18 15:36:19 -0500

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+# ChombyBot
+A virtual pet for your Discord server.
+
+"Are you looking for a friend? Someone to walk on the beaches of Azeroth with? Me too! How about a cuddle on the couch and someone who will listen and not judge you while you cut 1,000 yew logs? So am I! I’m not looking for just anyone. Someone who will appreciate my protective nature (no door bells needed!)! I’m not only a little cutie with my gnarled teeth and big brown eyes, I have substance and intelligence! I have learned the basics like getting the door, fetch, can drink water, and would love to continue learning new things." - Chomby
+
+
+Add Chomby to your Discord server today by:
+1) STEPS TO INVITE CHOMBY TO YOUR DISCORD.
+
+Commands Chomby is trained to respond to:
+  baddog   Sad Chomby
+  chomby   Call Chomby over
+  dingdong Someone just rang the doorbell..
+  feed     Give Chomby a snack
+  find     
+  shoo     Shoo Chomby away
+  sic      @ a user to sic Chomby on them
+  whistle  Call Chomby
+​No Category:
+  help     Shows this message
+
+Type ..help command for more info on a command.
+You can also type ..help category for more info on a category.
+
+
+Help develop Chomby with the following commands to get started:
+1) STEPS TO INIT A LOCAL DEV ENV TO HELP CONTRIBUTE TO THE PROJECT.
+
+
+Love,
+
+Chomby
---
+++ main.py
@@ -0,0 +1,139 @@
+import discord
+from discord.ext import commands
+import asyncio
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Dog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def find(self, ctx):
+        users = ctx.guild.voice_channels
+
+        userlist = []
+        for user in users:
+            for u in user.members:
+                print(u.name)
+    
+    @commands.command()
+    async def sic(self, ctx, member: discord.Member):
+        """ @ a user to sic Chomby on them"""
+
+        try:
+            voice_channel = member.voice.channel
+
+            if ctx.voice_client is not None:
+                return await ctx.voice_client.move_to(voice_channel)
+            await voice_channel.connect()
+            source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio('bark2.wav'))
+            ctx.voice_client.play(source)
+
+            while ctx.voice_client.is_playing():
+                await asyncio.sleep(1)
+            
+            ctx.voice_client.stop()
+            await ctx.voice_client.disconnect()
+        except:
+            await ctx.send("User not in a voice channel")
+        
+
+
+
+    @commands.command(help="Call Chomby over")
+    async def chomby(self, ctx, *, query='bark2.wav'):
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
+        ctx.voice_client.play(source)
+
+        while ctx.voice_client.is_playing():
+            await asyncio.sleep(1)
+        
+        ctx.voice_client.stop()
+        await ctx.voice_client.disconnect()
+
+
+    @commands.command(help="Give Chomby a snack")
+    async def feed(self, ctx, *, query='feeding.mp3'):
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
+        ctx.voice_client.play(source)
+
+        while ctx.voice_client.is_playing():
+            await asyncio.sleep(1)
+        
+        ctx.voice_client.stop()
+        await ctx.voice_client.disconnect()
+
+    @commands.command(help="Someone just rang the doorbell..")
+    async def dingdong(self, ctx, *, query='dingdong.mp3'):
+
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
+        ctx.voice_client.play(source)
+
+        while ctx.voice_client.is_playing():
+            await asyncio.sleep(1)
+
+        ctx.voice_client.stop()
+        await ctx.voice_client.disconnect()
+    
+    @commands.command()
+    async def baddog(self, ctx, *, query='whine.wav'):
+        """Sad Chomby"""
+
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
+        ctx.voice_client.play(source)
+
+        while ctx.voice_client.is_playing():
+            await asyncio.sleep(1)
+
+        ctx.voice_client.stop()
+        await ctx.voice_client.disconnect()
+        await ctx.send("bitch")
+
+
+    @commands.command()
+    async def whistle(self, ctx):
+        """Call Chomby"""
+
+        await ctx.channel.connect()
+
+    @whistle.before_invoke
+    @chomby.before_invoke
+    @feed.before_invoke
+    @dingdong.before_invoke
+    @baddog.before_invoke
+    async def ensure_voice(self, ctx):
+        if ctx.voice_client is None:
+            if ctx.author.voice:
+                await ctx.author.voice.channel.connect()
+            else:
+                await ctx.send("Chomby can't find you (You need to be in a voice channel)")
+
+    @commands.command()
+    async def shoo(self, ctx):
+        """Shoo Chomby away"""
+        await ctx.voice_client.disconnect()
+
+
+class Chomby(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('..'), description="It's Chomby!", intents=intents)
+
+
+
+@bot.event
+async def on_ready():
+    print('{0} is ready'.format(bot.user))
+
+
+bot.add_cog(Dog(bot))
+
+bot.run(os.getenv('TOKEN'))
